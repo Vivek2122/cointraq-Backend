478592bbba8068ccd309179b69ddf0f29eb9a062
Vivek2122 <vpal21324@gmail.com>
2025-06-14 11:47:30 -0400

    back fix

--- index.mjs
+++ index.mjs
@@ -64,15 +64,14 @@ app.use("/transaction", transactionRouter);
 app.use("/auth", authRouter);
 app.use("/", userRouter);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
 	console.log(`server listening at port ${PORT}`);
